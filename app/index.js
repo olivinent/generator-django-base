83fcb559a85bfd11ed62892a110120a5179d3f51
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-09 18:27:38 +0300

    Create production settings file

--- app/index.js
+++ app/index.js
@@ -173,6 +173,7 @@ DjangoGenerator.prototype.project = function project() {
     this.template('project/settings/base.py', path.join(projectFolder, 'settings/base.py'));
     this.template('project/settings/local.py', path.join(projectFolder, 'settings/local.py'));
     this.copy('project/settings/test.py', path.join(projectFolder, 'settings/test.py'));
+    this.copy('project/settings/production.py', path.join(projectFolder, 'settings/production.py'));
 };
 
 DjangoGenerator.prototype.baseApp = function baseApp() {

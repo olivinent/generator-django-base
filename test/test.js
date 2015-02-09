680c8bfb5b9014347dc7d3fc78e1427b307a5c8c
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-09 23:46:39 +0300

    Add first version of tests for project structure
    Needs some fine tuning. Failing for now

--- test/test.js
+++ test/test.js
@@ -2,7 +2,64 @@
 
 var path    = require('path');
 var helpers = require('yeoman-generator').test;
+var assert = require('assert');
 
 describe('Django base generator test', function () {
-    // TODO
+  var mockPrompts = {
+    database: 'sqlite3',
+  };
+
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.app = helpers.createGenerator('django-base', [
+        '../../app'
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      // Dot files and others
+      '.gitignore',
+      'package.json',
+      'bower.json',
+      'README.md',
+      'requirements.txt',
+
+      // General Python files
+      'urls.py',
+      'wsgi.py',
+      'manage.py',
+      '__init__.py',
+
+      // Settings files
+      'settings/__init__.py',
+      'settings/common.py',
+      'settings/testing.py',
+      'settings/development.py',
+      'settings/production.py',
+
+      // Requirements files
+      'requirements/base.txt',
+      'requirements/test.txt',
+      'requirements/local.txt',
+      'requirements/production.txt'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      database: 'sqlite3',
+      projectName: 'test_project',
+    });
+
+    this.app.options['skip-install'] = true;
+    this.app.on('end', function() {
+      helpers.assertFiles(expected);
+      done();
+    }).run({}, function(){});
+  });
 });

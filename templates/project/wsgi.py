8a6fc2b6e01f6ab6f3501e9d2a795c22733e22ad
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-09 18:54:29 +0300

    Use project name as is in wsgi.py
    Don't camelize project name
    This creates non-existent paths

--- templates/project/wsgi.py
+++ templates/project/wsgi.py
@@ -24,7 +24,7 @@ path.append(SITE_ROOT)
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 os.environ['CELERY_LOADER'] = 'django'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<%= _.camelize(projectName) %>.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<%= projectName %>.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION

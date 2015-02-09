f6080f6c28622674e6751b0d1da5d41218f67e0b
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-09 20:23:25 +0300

    Build sqlite db name from databaseName and not projectName

--- templates/project/settings/local.py
+++ templates/project/settings/local.py
@@ -29,7 +29,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': normpath(join(DJANGO_ROOT, '<%= _.camelize(projectName) %>.db')),
+        'NAME': normpath(join(DJANGO_ROOT, '<%= _.camelize(databaseName) %>.db')),
     }
 }
 <% } else if (dbMysql) { %>
